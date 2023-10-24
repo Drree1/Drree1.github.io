98f3ac7955351a6cb8d0b9d000077355b6e5e7c9
Drree1 <Dwgraham@email.sc.edu>
2023-10-23 20:29:08 -0400

    edited website and finished part 4

--- projects/part3/script.js
+++ projects/part3/script.js
@@ -2,6 +2,32 @@ const toggleNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
   };
 
+  const showHome = () => {
+    document.getElementById("index.html").classList.add("");
+}
+
+const showInfo = () => {
+    document.getElementById("index.html").classList.add("hide");
+    document.getElementById("info.html").classList.remove("hide");
+}
+
+const showHospital = () => {
+  document.getElementById("square").classList.add("move-square");
+}
+
+const showWait = () => {
+  document.getElementById("jasper") .classList.remove("hide");
+}
+
+const showThanks = () => {
+  document.getElementById("jasper") .classList.remove("hide");
+}
+
+const hideJasper = () => {
+    document.getElementById("jasper") .classList.add("hide");
+}
+
   window.onload = () => {
     document.getElementById("hamburger").onclick = toggleNav;
+    document.getElementById("button-next").onclick = showInfo;
 }

ad5562beafa5bfe83e15040eb5124172ed12fdfe
Drree1 <Dwgraham@email.sc.edu>
2023-10-29 21:43:13 -0400

    added json file

--- projects/part5/script.js
+++ projects/part5/script.js
@@ -1,10 +1,7 @@
 const toggleNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
   };
-
-  fetch('https://server.com/index.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  
   
 
 

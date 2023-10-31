310f59445f612d4048d9ad7f855a71caefd3ee2c
Drree1 <Dwgraham@email.sc.edu>
2023-10-30 21:33:29 -0400

    added json file

---
+++ projects/part5/about.js
@@ -0,0 +1,52 @@
+const getIndex = async () => {
+    const url = " https://raw.githubusercontent.com/Drree1/Drree1.github.io/main/json/about.json";
+  
+    try {
+      const response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const showIndex = async () => {
+    let index = await getIndex();
+    let indexSection = document.getElementById("index-section");
+  
+    index.forEach((index) => {
+      indexSection.append(getIndexItem(index));
+    });
+  };
+  
+  const getIndexItem = (index) => {
+    let section = document.createElement("section");
+  
+    let h3 = document.createElement("h3");
+    h3.innerText = index.title;
+    let ul = document.createElement("ul");
+    section.append(ul);
+    ul.append(getLi(index.title3));
+    ul.append(getLi(index.description7));
+    ul.append(getLi(index.description8));
+    ul.append(getLi(index.description9));
+    ul.append(getLi(index.description10));
+    ul.append(getLi(index.title4));
+    ul.append(getLi(index.description11));
+    ul.append(getLi(index.description12));
+    ul.append(getLi(index.description13));
+    ul.append(getLi(index.description14));
+  
+  
+    return section;
+
+  };
+  
+  const getLi = (data) => {
+    const li = document.createElement("li");
+    li.textContent = data;
+    return li;
+  };
+  
+  
+  window.onload = () => showIndex();
+  

d1b38f3a2b79365edad6da6306d2594bb85d4cb6
Drree1 <Dwgraham@email.sc.edu>
2023-12-08 20:23:05 -0500

    added final project

---
+++ projects/Final/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const mongoose = require("mongoose");
